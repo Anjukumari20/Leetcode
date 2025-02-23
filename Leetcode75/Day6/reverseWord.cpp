118f80adceb7cf1aae590d31b5cb80e59f863de7
anjuk <anjukumari0720@gmail.com>
2025-02-23 17:25:18 +0530

    Reverse Words in a String

---
+++ Leetcode75/Day6/reverseWord.cpp
@@ -0,0 +1,17 @@
+stringstream ss(s);
+string word;
+vector<string> words;
+
+while (ss >> word) {  // Extract words while ignoring extra spaces
+    words.push_back(word);
+}
+
+reverse(words.begin(), words.end());  // Reverse the order of words
+
+string result;
+for (int i = 0; i < words.size(); i++) {
+    if (i > 0) result += " ";  // Add space between words
+    result += words[i];
+}
+
+return result;
