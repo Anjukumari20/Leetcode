e39c963399360d64b8872cf1c3ad686c40c3dcf5
anjuk <anjukumari0720@gmail.com>
2025-03-11 12:19:57 +0530

    1657. Determine if Two Strings Are Close

---
+++ Leetcode75/Day22/determine.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        bool closeStrings(string word1, string word2) {
+            if (word1.size() != word2.size()) return false;
+    
+            vector<int> freq1(26, 0), freq2(26, 0);
+            set<char> set1, set2;
+    
+            for (char c : word1) {
+                freq1[c - 'a']++;
+                set1.insert(c);
+            }
+            for (char c : word2) {
+                freq2[c - 'a']++;
+                set2.insert(c);
+            }
+    
+            if (set1 != set2) return false; // Unique characters must be the same
+    
+            sort(freq1.begin(), freq1.end());
+            sort(freq2.begin(), freq2.end());
+    
+            return freq1 == freq2;
+        }
+    };
